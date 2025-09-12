34a9c5b01201ee3c6da9c9a6e9b4209a9c48ad31
nikita74939 <itanikita20005@gmail.com>
2025-09-13 06:31:51 +0700

    nambahin route buat create update delete

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,25 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { CLogin } from "../controllers/auth.controller";
+import { Router } from "express";
+import { 
+  CLogin, 
+  CCreateAdmin, 
+  CUpdateAdmin, 
+  CDeleteAdmin 
+} from "../controllers/auth.controller";
+import { validateRequest } from "../middlewares/validation.middleware";
+import { 
+  createAdminSchema, 
+  updateAdminSchema, 
+  loginSchema 
+} from "../middlewares/validation.middleware";
 
 const router = Router();
 
-router.post("/login", CLogin);
+router.post("/login", validateRequest(loginSchema), CLogin);
+
+router.post("/create", validateRequest(createAdminSchema), CCreateAdmin);
+
+router.put("/:id", validateRequest(updateAdminSchema), CUpdateAdmin);
+
+router.delete("/:id", CDeleteAdmin);
 
 export default router;
